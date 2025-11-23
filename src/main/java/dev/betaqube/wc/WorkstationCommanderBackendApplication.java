98500d7e14cbe03d101958738618b332dab2122b
Samuel André <samuel.asilva@icloud.com>
2025-11-23 01:31:52 -0300

    chore: initial project import

---
+++ src/main/java/dev/betaqube/wc/WorkstationCommanderBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.betaqube.wc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkstationCommanderBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkstationCommanderBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/betaqube/wc/WorkstationCommanderBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.betaqube.wc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkstationCommanderBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
